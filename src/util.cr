0baf0454d75fbf195f0e0fab692ea81491c5cc66
RX14 <chris@rx14.co.uk>
2017-03-09 00:12:17 +0000

    Add XDG application dirs helper

---
+++ src/util.cr
@@ -0,0 +1,33 @@
+module Crane::Util
+  record ApplicationDirectory,
+    config : String,
+    data : String,
+    cache : String
+
+  def self.get_application_dirs(name = nil)
+    # NOTE: See Haskell for windows implementations https://hackage.haskell.org/package/directory-1.3.1.0/docs/System-Directory.html#t:XdgDirectory
+    config = ENV["XDG_CONFIG_HOME"]?
+    config = File.expand_path("~/.config") unless config && config.starts_with?(File::SEPARATOR)
+    config = File.join(config, name) if name
+
+    data = ENV["XDG_DATA_HOME"]?
+    data = File.expand_path("~/.local/share") unless data && data.starts_with?(File::SEPARATOR)
+    data = File.join(data, name) if name
+
+    cache = ENV["XDG_CACHE_HOME"]?
+    cache = File.expand_path("~/.cache") unless cache && cache.starts_with?(File::SEPARATOR)
+    cache = File.join(cache, name) if name
+
+    ApplicationDirectory.new(config, data, cache)
+  end
+
+  def self.ensure_application_dirs(name)
+    dirs = get_application_dirs(name)
+
+    Dir.mkdir(dirs.config)
+    Dir.mkdir(dirs.data)
+    Dir.mkdir(dirs.cache)
+
+    dirs
+  end
+end
