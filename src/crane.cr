5392c79344899a5e5ed012fcad452069c8cb1cd0
RX14 <chris@rx14.co.uk>
2017-03-18 18:34:24 +0000

    Fix requiring crane requiring entrypoint
    We must selectively require so that we don't require entrypoint.cr which is the
    entrypoint for the crane command and contains top-level code.

--- src/crane.cr
+++ src/crane.cr
@@ -1,6 +1,9 @@
-require "./**"
-
 module Crane
   VERSION     = "0.1.0"
   DESCRIPTION = "Manage and install crystal versions and toolchains with ease"
 end
+
+require "./util"
+require "./version_manager"
+require "./command"
+require "./tasks/**"
